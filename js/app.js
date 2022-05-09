bcc194d9b2ba64cb8ab9e6ac558eb0ad0212c6eb
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-05-09 09:07:37 -0400

    Added basic form functionality.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@
 /****************************/
 
 // Add the event listener constants.
-const newRow = document.getElementById('newRow');
+const newRow = document.getElementById('form');
 
 // Function to create a new row and insert it into the document.
 function createNewRow() {
-    console.log(newRow);
+    newRow.innerHTML += `<div>
+        <textarea rows="20" cols="60"></textarea>
+    </div>`
 }
 
 newRow.addEventListener('click', createNewRow);
