cfb49eb1fa2ca2f1aa66c03f82c2e3f4b140df2f
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-05-11 13:56:45 -0400

    Added the functionality for the "add new row" button. Added submit.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,27 @@
 /*** FAQ Schema Generator ***/
 /****************************/
 
+// Add global variables.
+let faqCount = 1;
+
 // Add the event listener constants.
 const newRow = document.getElementById('form');
 
 // Function to create a new row and insert it into the document.
 function createNewRow() {
-    newRow.innerHTML += `<div>
-        <textarea rows="20" cols="60"></textarea>
-    </div>`
+    newRow.innerHTML += `
+    <div class="faq-entry">
+        <div>
+            <label for="faqQuestion${faqCount}">Question ${faqCount}</label>
+            <input type="text" id="faqQuestion${faqCount}">
+        </div>
+        <div>
+            <label for="faqAnswer${faqCount}">Answer ${faqCount}</label>
+            <textarea id="faqAnswer${faqCount}" rows="20" cols="60"></textarea>
+        </div>
+    </div>
+    `
+    faqCount = faqCount + 1;
 }
 
 newRow.addEventListener('click', createNewRow);
